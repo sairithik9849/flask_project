0003b7c9930450d4e5fcd82de76b85f03554b26f
sairithik9849 <ksairithik_cse200528@mgit.ac.in>
2024-11-28 18:37:21 -0500

    Implemented extra credit

--- api.py
+++ api.py
@@ -17,7 +17,8 @@ class RegisterUser(Resource):
 class GetUser(Resource):
     def get(self,user_id):
         user = users.get(user_id)
-        if user is None: return {"Message": "There is no user with the specified id"}
+        if user is None:
+            return {"Message": "There is no user with the specified id"}
         return {
             "id": user["id"],
             "name": user["name"],
@@ -48,16 +49,17 @@ class AddWorkout(Resource):
     def put(self,user_id):
         date,time,distance = AddWorkout_parser.parse_args().values()
         user = users.get(user_id)
-        if user is None: return {"Message": "There is no user with the specified id"}, 404
+        if user is None:
+            return {"Message": "There is no user with the specified id"}, 404
         if date is not None and time is not None and distance is not None:
             new_workout = {
                 "date": date,
                 "time": time,
                 "distance": distance
             }
-            if 'workouts' not in user:
-                user['workouts'] = []
-            user['workouts'].append(new_workout)
+            if "workouts" not in user:
+                user["workouts"] = []
+            user["workouts"].append(new_workout)
             return new_workout, 200
         else:
             return {"Message": "Missing required fields"}, 400
@@ -65,7 +67,41 @@ class AddWorkout(Resource):
 class ListWorkouts(Resource):
     def get(self,user_id):
         user = users.get(user_id)
-        if user is None: {"Message": "There is no user with the specified id"}, 404
-        workouts = user.get('workouts', [])
+        if user is None:
+            return {"Message": "There is no user with the specified id"}, 404
+        workouts = user.get("workouts", [])
         return {"workouts": [{"date": workout["date"],"distance": workout["distance"],"time": workout["time"]} for workout in workouts]}, 200
-    
+
+# Work on Extra Credit --
+FollowFriend_parser = reqparse.RequestParser().add_argument("follow_id",type=str,required=True)
+class FollowFriend(Resource):
+    def put(self,user_id):
+        user = users.get(user_id)
+        follow_id = FollowFriend_parser.parse_args()["follow_id"]
+        follow = users.get(follow_id)
+        if user is None or follow is None:
+            return {"Message": "There is no user with the specified id"}, 404
+        if "following" not in users[user_id]: users[user_id]["following"] = set()
+        users[user_id]["following"].add(follow_id)
+        return {"following": list(map(str,users[user_id]["following"]))}, 200
+
+class ShowFriendWorkouts(Resource):
+    def get(self,user_id,follow_id):
+        user = users.get(user_id)
+        follow = users.get(follow_id)
+        if user is None or follow is None: 
+            return {"Message": "There is no user with the specified id"}, 404
+        if "following" not in users[user_id] or follow_id not in users[user_id]["following"]:
+            return {"Message": "You are not following the specified user!"}, 403
+        friend_workouts = users[follow_id].get("workouts", [])
+        response = {
+            "workouts": [
+                {
+                    "date": workout["date"],
+                    "distance": workout["distance"],
+                    "time": workout["time"]
+                }
+                for workout in friend_workouts
+            ]
+        }
+        return response, 200
--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ from api import *
 base_route = "/user"
 user_id = "/user/<string:user_id>"
 workouts_user_id = "/workouts/<string:user_id>"
+follow = "/follow-list/<string:user_id>"
 
 routes = {
     RegisterUser: base_route,
@@ -11,7 +12,9 @@ routes = {
     RemoveUser: user_id,
     ListUsers: "/users",
     AddWorkout: workouts_user_id,
-    ListWorkouts: workouts_user_id
+    ListWorkouts: workouts_user_id,
+    FollowFriend: follow,
+    ShowFriendWorkouts: f"{follow}/<string:follow_id>"
 }
 
 methods = {
@@ -20,7 +23,9 @@ methods = {
     RemoveUser: "DELETE",
     ListUsers: "GET",
     AddWorkout: "PUT",
-    ListWorkouts: "GET"
+    ListWorkouts: "GET",
+    FollowFriend: "PUT",
+    ShowFriendWorkouts: "GET"
 }
 
 def init_routes(api):
